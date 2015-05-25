b2daa546f7b111215e82181736176a44fee56d56
solomon <1185626206@qq.com>
2015-05-25 15:11:34 +0800

    WordCount

--- src/JavaWordCount.java
+++ src/JavaWordCount.java
@@ -1,5 +1,65 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
+import org.apache.spark.api.java.function.PairFunction;
+import scala.Tuple2;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 /**
  * Created by Suheng on 5/25/15.
  */
 public class JavaWordCount {
+    //使用空格进行分词
+    private static final Pattern SPACE = Pattern.compile(" ");
+
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            // 如果没有给文件参数则退出程序
+            System.err.println("Usage: JavaWordCount<file>");
+            System.exit(1);
+        }
+
+        //设置Spark的环境变量及本机运行环境
+        SparkConf sparkConf = new SparkConf().setAppName("JavaWordCount").setMaster("local");
+        JavaSparkContext ctx = new JavaSparkContext(sparkConf);
+        JavaRDD<String> lines = ctx.textFile(args[0], 1);
+
+        //将每一行进行分词
+        JavaRDD<String> words = lines.flatMap(new FlatMapFunction<String, String>() {
+            @Override
+            public Iterable<String> call(String s) throws Exception {
+                return Arrays.asList(SPACE.split(s));
+            }
+        });
+
+        //将数据转换为<k,1>形式
+        JavaPairRDD<String, Integer> ones = words.mapToPair(new PairFunction<String, String, Integer>() {
+            @Override
+            public Tuple2<String, Integer> call(String s) throws Exception {
+                return new Tuple2<String, Integer>(s, 1);
+            }
+        });
+
+        //相当于reduce阶段，进行合并操作,同一个key进行累加
+        JavaPairRDD<String, Integer> counts = ones.reduceByKey(new Function2<Integer, Integer, Integer>() {
+            @Override
+            public Integer call(Integer integer, Integer integer2) throws Exception {
+                return integer + integer2;
+            }
+        });
+
+        //累计完毕后，对数据进行输出
+        List<Tuple2<String, Integer>> output = counts.collect();
+        for (Tuple2<String, Integer> tuple : output) {
+            System.out.println(tuple._1() + ": " + tuple._2());
+        }
+
+        ctx.stop();
+    }
 }
